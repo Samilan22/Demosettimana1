c00bd7a8ced388a32af4cc8bd18ad76789df773c
User ACD 07 <useracd07@DSACD007.local>
2018-02-22 11:07:51 +0100

    finito il task di copiatura

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 var jshint = require('gulp-jshint');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
 
 gulp.task('jshint', function(){
     return gulp.src('demosettimana1/*js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', function(){
+    return gutil.log('gulp è in esecuzione');
+});
+
+gulp.task('copiaHTML', function(){
+    gulp.src('demosettimana/*html').pipe(gulp.dest('distribuzione'));
+});
+
+gulp.task('copiaCSS', function(){
+    gulp.src('demosettimana/Style.css').pipe(gulp.dest('distribuzione'));
+});
+
+gulp.task('copiaJavaScript', function(){
+    gulp.src('demosettimana/*js').pipe(gulp.dest('distribuzione'));
 });
